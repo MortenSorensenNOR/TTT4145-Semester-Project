e2a5869c523d0de0b98c7b0a5d47a047f2dc0ab0
Morten Sorensen <morten.sorensen.04@gmail.com>
2026-02-17 15:33:15 +0100

    working?

--- Makefile
+++ Makefile
@@ -20,14 +20,15 @@ deps-system:
 	sudo pacman -S --needed base-devel git libxml2 bison flex cmake libusb avahi libaio
 
 libiio:
-	cd vendor/libiio && mkdir -p build && cd build && \
-		cmake .. && make -j$$(nproc) && sudo make install
+	cd vendor/libiio && git checkout v0.25 && mkdir -p build && cd build && \
+		cmake -DCMAKE_INSTALL_PREFIX=/usr/local .. && make -j$$(nproc) && sudo make install
 	sudo ldconfig
 
 libad9361:
 	cd vendor/libad9361-iio && mkdir -p build && cd build && \
 		cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
-			-DLIBIIO_INCLUDEDIR=$$(pkg-config --variable=includedir libiio)/iio \
+			-DLIBIIO_INCLUDEDIR=/usr/local/include \
+			-DLIBIIO_LIBRARIES=/usr/local/lib/libiio.so \
 			.. && make -j$$(nproc) ad9361 && \
 		sudo cp -P libad9361.so* /usr/local/lib/ && \
 		sudo cp ../ad9361.h /usr/local/include/ && \
--- setup.sh
+++ setup.sh
@@ -4,10 +4,11 @@ set -e
 echo "==> Initializing submodules..."
 git submodule update --init --recursive
 
-echo "==> Building libiio..."
+echo "==> Building libiio v0.25..."
 cd vendor/libiio
+git checkout v0.25
 mkdir -p build && cd build
-cmake ..
+cmake -DCMAKE_INSTALL_PREFIX=/usr/local ..
 make -j$(nproc)
 sudo make install
 sudo ldconfig
@@ -16,8 +17,10 @@ cd ../../..
 echo "==> Building libad9361-iio..."
 cd vendor/libad9361-iio
 mkdir -p build && cd build
-LIBIIO_INCLUDE=$(pkg-config --variable=includedir libiio)/iio
-cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DLIBIIO_INCLUDEDIR=$LIBIIO_INCLUDE ..
+cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+    -DLIBIIO_INCLUDEDIR=/usr/local/include \
+    -DLIBIIO_LIBRARIES=/usr/local/lib/libiio.so \
+    ..
 make -j$(nproc) ad9361
 sudo cp -P libad9361.so* /usr/local/lib/
 sudo cp ../ad9361.h /usr/local/include/
