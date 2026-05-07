27c82a34f0ea416af91fb575d07e5c76573218af
Morten Sorensen <morten.sorensen.04@gmail.com>
2026-05-07 17:55:53 +0200

    change buffer sizing to just be a multiple of 8, not a power of two

--- utils/bit.py
+++ utils/bit.py
@@ -1,3 +1,6 @@
+def round_up(n: int, m: int = 8) -> int:
+      return ((n + m - 1) // m) * m
+
 def int_to_bits(n: int, length: int) -> list[int]:
     """Convert an integer to a fixed-width big-endian bit list."""
     return [(n >> (length - 1 - i)) & 1 for i in range(length)]
